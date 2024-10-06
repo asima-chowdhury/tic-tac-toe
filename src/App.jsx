ee8923e67e952b7572f51ade6b1e84e6f5ac8afa
asima-chowdhury <asimachowdhury.lu@gmail.com>
2024-10-06 22:58:05 +0600

    passed event handlers as props

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@
 */
 import { useState } from "react";
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
@@ -17,10 +20,15 @@ function Square({ value }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} />
         <Square value={squares[2]} />
       </div>
