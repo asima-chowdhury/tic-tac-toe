5e694b29e83c63f11a3cc5416f03898c0f379907
asima-chowdhury <asimachowdhury.lu@gmail.com>
2024-10-06 22:44:21 +0600

    sqaure, board component created, added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
-export default function App() {
-  return <h1 className="text-3xl fond-bold underline">Hello world</h1>;
+/*
+   Game
+    -> Board
+        -> Square
+    -> History
+*/
+import { useState } from "react";
+
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="flex">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
+  );
 }
